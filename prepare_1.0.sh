33fef2a7d53922e54800b25e7f9df40100ff77e9
ddonatien <35450772+ddonatien@users.noreply.github.com>
2024-09-02 12:46:41 +0200

    Update prepare_1.0.sh

--- prepare_1.0.sh
+++ prepare_1.0.sh
@@ -1,5 +1,4 @@
 conda activate defo
 export PYFLEXROOT=${PWD}/PyFlex
-export PYTHONPATH=${PYFLEXROOT}/bindings/build:$PYTHONPATH
-export PYTHONPATH=${PWD}:$PYTHONPATH
+export PYTHONPATH=${PWD}:${PYFLEXROOT}/bindings/build:$PYTHONPATH
 export LD_LIBRARY_PATH=${PYFLEXROOT}/external/SDL2-2.0.4/lib/x64:$LD_LIBRARY_PATH
