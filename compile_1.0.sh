3f1a84c539fbe2b81cd40eb5e33ef666074609af
ddonatien <35450772+ddonatien@users.noreply.github.com>
2024-09-20 12:20:27 +0200

    Update compile_1.0.sh

--- compile_1.0.sh
+++ compile_1.0.sh
@@ -5,10 +5,11 @@ cd build
 
 if [[ $(hostname) = *"franklin"* ]]; then
   export CUDA_BIN_PATH=/usr/local/cuda-12.1
+  cmake -DPYBIND11_PYTHON_VERSION=3.8 ..
 else
   export CUDA_BIN_PATH=/usr/local/cuda-9.1
+  cmake -DPYBIND11_PYTHON_VERSION=3.6 ..
 fi
 
-cmake -DPYBIND11_PYTHON_VERSION=3.6 ..
 make -j
 cd ../../..
